bbf205dd3fe2b9dd50ec3a43548ae120daa1c274
Abu Bin Fahd <78264928+abubinfahd@users.noreply.github.com>
2021-06-10 22:39:11 +0600

    Add files via upload
    Only those women who paid more than 20 & 3rd class tickets died

---
+++ 3rd-prediction.R
@@ -0,0 +1,75 @@
+# Set the working directory
+
+setwd ("G:/datasets/titanic")
+
+# Import the training set: train
+# Your working directory might vary
+
+train <- read.csv ("G:/datasets/titanic/train.csv")
+
+# Import the test set too -> test dataframe
+# Your working directory might vary
+
+test <- read.csv("G:/datasets/titanic/test.csv")
+
+# Need to create subsets of Fare
+
+train$Fare2 <- '30+'
+train$Fare2[train$Fare < 30 & train$Fare >= 20] <- '20-30'
+train$Fare2[train$Fare < 20 & train$Fare >= 10] <- '10-20'
+train$Fare2[train$Fare < 10] <- '<10'
+
+# some tests, do as you feel like
+
+table(train$Sex, train$Survived)
+
+table(train$Sex)
+
+aggregate(Survived ~ Sex, data=train, FUN=sum)
+
+aggregate(Survived ~ Sex, data=train, FUN=length)
+
+aggregate(Survived ~ Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+
+# back to 3rd prediction
+
+# Comparing more than two subsets, we need to use command "aggregate"
+
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=sum)
+
+# Let's look at actually how many were they in each subset
+
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=length)
+
+# comparing subsets in terms of FUN=function(x) {sum(x)/length(x)}
+
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)}) 
+
+# In percentage
+
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x) * 100})
+
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+
+# Create new column in test set with our prediction that everyone dies
+test$Survived <- 0
+# Update the prediction to say that all females will survive
+test$Survived[test$Sex == 'female'] <- 1
+# Update once more to say that females who pay more for a third class fare also perish
+test$Survived[test$Sex == 'female' & test$Pclass == 3 & test$Fare >= 20] <- 0
+
+# send the dataframe with two columns   
+
+prediction3rd <- data.frame (test$PassengerId, test$Survived)
+
+# naming the columns
+
+names (prediction3rd) <- c("PassengerId","Survived")
+
+# no rownames
+
+rownames (prediction3rd) <- NULL
+
+# Finally, time to submit it to Kaggle.com
+
+write.csv (prediction3rd, file = "prediction3.csv", row.names=FALSE)
